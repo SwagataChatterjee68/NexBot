2385e9fea66ad38a400a35278025bcc339b2bb16
SwagataChatterjee68 <ranichatterjee21112@gmail.com>
2025-08-17 22:56:03 +0530

    refactor: remove unused view engine and static file middleware from app.js; clean up socket connection logging

--- src/app.js
+++ src/app.js
@@ -5,11 +5,7 @@ const chatRoutes = require("./routes/chat.routes");
 const cookieParser = require("cookie-parser");
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/auth", authRoutes);
--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -4,7 +4,7 @@ function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
 
   io.on("connection", (socket) => {
-    console.log("New socket connection:", socket.id);
+    
   });
 }
 
